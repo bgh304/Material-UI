bd1d5283b148dab19e9269791d38f94cea708615
bgh304 <bgh304@myy.haaga-helia.fi>
2020-03-16 17:50:30 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './App.css';
+import Todolist from './components/Todolist';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            My Todos
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Todolist />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todolist.js
@@ -0,0 +1,65 @@
+import React, {useState} from 'react';
+import '../App.css';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import Tooltip from '@material-ui/core/Tooltip';
+
+export default function Todolist() {
+
+  const [todo, setTodo] = useState({desc: '', date: ''}); //useStaten sisällä on OLIO!
+  const [todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+      setTodo({...todo, [event.target.name]: event.target.value}); //asettaa OLIO-arvon!
+  }
+
+  const addTodo = () => {
+    setTodos([todo, ...todos]); //hakasulut käsittelee taulukkona!
+    setTodo({desc: '', date: ''});
+  }
+
+  /*const addTodo = (event) => {
+      event.preventDefault();
+      setTodos([...todos, todo]); //hakasulut käsittelee taulukkona!
+      setTodo({desc: '', date: ''});
+  }*/
+
+  const deleteTodo = (row) => {
+    console.log(row);
+    setTodos(todos.filter((todo, index) => index !== row));
+  }
+
+  const columns = [
+    {
+      Header: 'Description',
+      accessor: 'desc'
+    },
+    {
+      Header: 'Date',
+      accessor: 'date'
+    },
+    {
+      filterable: false,
+      sortable: false,
+      minWidth: 30,
+      Cell: row => <Button size="small" color="secondary" onClick={() => deleteTodo(row.index)}>Delete</Button>
+    }
+  ]
+  
+    return (
+      <div>
+        <TextField style={{marginRight: 15}} label="Description" value={todo.desc} name="desc" onChange={inputChanged} />
+        <TextField style={{marginRight: 15}} label="Date" name="date" value={todo.date} onChange={inputChanged} />
+        <Tooltip title="Add Todo">
+        <IconButton style={{marginTop: 10}} onClick={addTodo}>
+          <AddCircleIcon color="primary" fontSize="large"/>
+        </IconButton>
+        </Tooltip>
+        <ReactTable columns={columns} date={todos} defaultPageSize={10} filterable={true}/>
+      </div>
+    );
+}//delete todos.mappiin
